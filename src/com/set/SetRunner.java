08f983795a9cbfc5103adf61d7ee2be2041d489a
Alzhan <aismagulov@gmail.com>
2022-10-02 21:25:22 +0600

    Commit 48. HashSet

--- src/com/set/SetRunner.java
+++ src/com/set/SetRunner.java
@@ -4,18 +4,32 @@ import java.util.*;
 
 public class SetRunner {
     public static void main(String[] args) {
-        String[] cars = {"Mersedes", "Mini", "Audi", "VW", "Smart", "Toyota"};
-        String[] otherCars = { "Audi", "Ford", "GMS", "Toyota", "Chevrolet"};
+        Set<Car> sixCars = new HashSet<>();
+        sixCars.add(new Car("VW", "Golf", 45));
+        sixCars.add(new Car("Audi", "A3", 60));
+        sixCars.add(new Car("VW", "Polo", 25));
+        sixCars.add(new Car("BMW", "Z4", 120));
+        sixCars.add(new Car("BMW", "440i", 200));
 
-        Set<String> carSet = new TreeSet<>(Arrays.asList(cars));
-        Set<String> otherCarSet = new TreeSet<>(Arrays.asList(otherCars));
-        Set<String> uniqueCars = new TreeSet<>(carSet);
-        uniqueCars.addAll(otherCarSet);
+        Set<Car> europaCars = new HashSet<>();
+        europaCars.add(new Car("Toyota", "Auris", 40));
+        europaCars.add(new Car("Renault", "Clio", 30));
+        europaCars.add(new Car("Renault", "Megan", 50));
+        europaCars.add(new Car("VW", "Golf", 45));
+        europaCars.add(new Car("VW", "Polo", 35));
 
+        Set<Car> uniqueCars = new HashSet<>(sixCars);
+        uniqueCars.addAll(europaCars);
         print(uniqueCars);
+
+
     }
 
-    private static void print(Set<String> cars){
+    private static void print(Set<Car> cars){
+        System.out.printf("%-20s %-20s %-20s \n", "Car brand", "Model", "Price per day");
+        for (Car car: cars) {
+            System.out.printf("%-20s %-20s %-20s \n", car.getCarBrand(), car.getModel(), car.getPricePerDay());
+        }
         System.out.println(cars);
     }
 }
